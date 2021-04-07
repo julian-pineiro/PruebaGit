f10bd33e52e9eef9966ac66ec919a0ac5f4f3204
Julian Pineiro <julipineiro@gmail.com>
2021-04-07 12:46:39 -0300

    Create Prueba posta.java

---
+++ Prueba posta.java
@@ -0,0 +1,20 @@
+public static void main(int args[]){
+	
+	public int suma(int i, int j){
+	// completar código @kari
+	}
+	
+	public int resta(int i, int j){
+	// completar código @tomi
+	}		
+	
+	public int dividir(int i, int j){
+	// completar código @mari
+	}	
+
+	public int mult(int i, int j){
+	// completar código @juli
+	}	
+
+
+}
